e94579857eb59d00b199b30f0c6b6d4992d6521a
LMO GerritBot <gerrit@libremobileos.com>
2024-05-24 04:08:48 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_aura.mk
+    $(LOCAL_DIR)/lmodroid_aura.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_aura-user \
-    lineage_aura-userdebug \
-    lineage_aura-eng
+    lmodroid_aura-user \
+    lmodroid_aura-userdebug \
+    lmodroid_aura-eng
--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ TARGET_SCREEN_WIDTH := 1440
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
